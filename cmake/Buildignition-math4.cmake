8f66b01b0093f1b248e69ef771d477069e371ff9
Silvio Traversaro <pegua1@gmail.com>
2018-01-29 23:27:15 +0100

    Declare dependency of ignition-math4 on ignition-cmake0

--- cmake/Buildignition-math4.cmake
+++ cmake/Buildignition-math4.cmake
@@ -1,10 +1,11 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
-include(YCMEPHelper)
+find_or_build_package(ignition-cmake0 QUIET)
 
 ycm_ep_helper(ignition-math4 TYPE HG
                              STYLE BITBUCKET
                              REPOSITORY ignitionrobotics/ign-math
                              TAG ign-math4
+                             DEPENDS ignition-cmake0
                              COMPONENT osrf)
